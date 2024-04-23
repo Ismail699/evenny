dec18a8d4f10cb8474a7147596d5bf34edc2afc1
John Doe <ismailmarouani20@gmail.com>
2024-04-23 02:29:25 +0200

    test webhook

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,8 @@
 import Card from "@/components/shared/Card";
 import Footer from "@/components/shared/Footer";
 import Header from "@/components/shared/Header";
-import { createUser } from "@/lib/actions/user.actions";
-import User from "@/lib/database/models/user.model";
-import { connectToDatabase } from "@/lib/database";
-import { currentUser, clerkClient } from "@clerk/nextjs/server";
+import { SyncUser } from "@/lib/utils";
 
-// const { onSignIn } = useClerk();
-// setTimeout(SyncUser, 2000);
 SyncUser();
 export default function RootLayout({
   children,
@@ -22,57 +17,4 @@ export default function RootLayout({
       <Footer />
     </div>
   );
-}
-
-async function SyncUser() {
-  currentUser()
-    .then(async (currentClerkUser) => {
-      await connectToDatabase().then(async () => {
-        const user = {
-          clerkId:
-            currentClerkUser?.id.toString() !== undefined
-              ? currentClerkUser?.id.toString()
-              : "id",
-          email: currentClerkUser?.emailAddresses[0]?.emailAddress
-            ? currentClerkUser.emailAddresses[0].emailAddress
-            : "emailAddress",
-          username: currentClerkUser?.username
-            ? currentClerkUser?.username!
-            : "username",
-          firstName: currentClerkUser?.firstName
-            ? currentClerkUser?.firstName
-            : "firstName",
-          lastName: currentClerkUser?.lastName
-            ? currentClerkUser?.lastName!
-            : "lastName",
-          photo: currentClerkUser?.imageUrl
-            ? currentClerkUser?.imageUrl!
-            : "photo",
-        };
-
-        const findUser = await User.findOne({
-          clerkId: user?.clerkId,
-        });
-
-        if (!findUser) {
-          const newUser = await createUser(user);
-          console.log("user created");
-          if (newUser) {
-            await clerkClient.users.updateUserMetadata(
-              currentClerkUser?.id.toString() !== undefined
-                ? currentClerkUser?.id.toString()
-                : "",
-              {
-                publicMetadata: {
-                  userId: newUser._id,
-                },
-              }
-            );
-          }
-        }
-      });
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
 }
